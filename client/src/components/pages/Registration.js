28c517724ed7cdcf97a226ca9d4263c8553029d1
MaksimKazak <kazakmaksims@gmail.com>
2019-12-10 17:04:39 +0300

    Start adding registration page

--- client/src/components/pages/Registration.js
+++ client/src/components/pages/Registration.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
+import Box from '@material-ui/core/Box';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
 function Registration() {
-  return <div>Registration</div>;
+  return (
+    <div>
+      Registration
+      <Box className='registration-box'>
+        <TextField className='registration-input' label='email'/>
+        <TextField className='registration-input' label='password' type='password'/>
+        <Button color='primary' variant='contained'>Sign up!</Button>
+      </Box>
+    </div>
+  );
 }
 
 export default Registration;
