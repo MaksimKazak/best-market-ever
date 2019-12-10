046f1bbf69ccd91572ffe2dcb19dd19e369c9edb
MaksimKazak <kazakmaksims@gmail.com>
2019-12-10 16:08:23 +0300

    Add phrase to main page

--- client/src/components/pages/Main.js
+++ client/src/components/pages/Main.js
@@ -1,9 +1,30 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
+
 function Main() {
+
+  let isAuthenticated = true;
+  let products = ['wood', 'iron', 'oil'];
+
   return (
     <div className='main-page'>
-      This application allows you to do one of the most important things - buy imaginary wood, iron and oil.
+      <p>
+        This application allows you to do one of the most important things - buy imaginary wood, iron and oil.
+      </p>
+      {
+        isAuthenticated ?
+          <p>
+            It's time to buy some { products[Math.floor(Math.random()*products.length)] }!
+          </p>
+          :
+          <p>
+            <Link to='/registration' className='link'>Sign up</Link>/<Link to='/authentication' className='link'>Sign
+            in</Link>
+            &nbsp;or try&nbsp;
+            <Link to='/operations' className='link'>demo</Link>.
+          </p>
+      }
     </div>
   );
 }
