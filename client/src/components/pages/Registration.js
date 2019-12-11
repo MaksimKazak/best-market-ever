133dbda7b71d353ff27d61beb971a48ef047a22c
MaksimKazak <kazakmaksims@gmail.com>
2019-12-11 11:04:28 +0300

    Add username field and add margins

--- client/src/components/pages/Registration.js
+++ client/src/components/pages/Registration.js
@@ -3,12 +3,14 @@ import React from 'react';
 import Box from '@material-ui/core/Box';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
 function Registration() {
   return (
     <div>
-      Registration
+      <Typography variant='h4' className='registration-header'>Registration</Typography>
       <Box className='registration-box'>
+        <TextField className='registration-input' label='username'/>
         <TextField className='registration-input' label='email'/>
         <TextField className='registration-input' label='password' type='password'/>
         <Button color='primary' variant='contained'>Sign up!</Button>
