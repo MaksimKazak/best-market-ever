7857885d667fbb485873ad1e4297c6c14ee6569c
MaksimKazak <kazakmaksims@gmail.com>
2019-12-11 15:13:39 +0300

    Add spacings and start making portfolio

--- client/src/components/elements/Operations.js
+++ client/src/components/elements/Operations.js
@@ -3,32 +3,40 @@ import React from 'react';
 import Grid from '@material-ui/core/Grid'
 import Paper from '@material-ui/core/Paper'
 import Button from '@material-ui/core/Button'
+import Typography from '@material-ui/core/Typography'
+import Divider from '@material-ui/core/Divider'
 
 function Operations() {
   return (
-    <Grid container spacing={6}>
-      <Grid item xs={4} spacing={16}>
-        <Paper className='box'>
-          <div>Wood</div>
-          <div>Price: 13.40$</div>
-          <Button variant='outlined' color='primary'>Buy</Button>
-        </Paper>
+    <div>
+      <Grid container spacing={6} className='space-bottom'>
+        <Grid item xs={4} spacing={16}>
+          <Paper className='box box-small-spacing'>
+            <Typography variant='h5' className='space-bottom'>Wood</Typography>
+            <Typography variant='p' component='div' className='space-bottom'>Price: 13.40$</Typography>
+            <Button variant='outlined' color='primary'>Buy</Button>
+          </Paper>
+        </Grid>
+        <Grid item xs={4} spacing={16}>
+          <Paper className='box box-small-spacing'>
+            <Typography variant='h5' className='space-bottom'>Iron</Typography>
+            <Typography variant='p' component='div' className='space-bottom'>Price: 30.50$</Typography>
+            <Button variant='outlined' color='primary'>Buy</Button>
+          </Paper>
+        </Grid>
+        <Grid item xs={4} spacing={16}>
+          <Paper className='box box-small-spacing'>
+            <Typography variant='h5' className='space-bottom'>Oil</Typography>
+            <Typography variant='p' component='div' className='space-bottom'>Price: 28.00$</Typography>
+            <Button variant='outlined' color='primary'>Buy</Button>
+          </Paper>
+        </Grid>
       </Grid>
-      <Grid item xs={4} spacing={16}>
-        <Paper className='box'>
-          <div>Iron</div>
-          <div>Price: 30.50$</div>
-          <Button variant='outlined' color='primary'>Buy</Button>
-        </Paper>
+      <Divider variant='middle' />
+      <Grid class='space-top-large'>
+        <Typography variant='h5'>Portfolio</Typography>
       </Grid>
-      <Grid item xs={4} spacing={16}>
-        <Paper className='box'>
-          <div>Oil</div>
-          <div>Price: 28.00$</div>
-          <Button variant='outlined' color='primary'>Buy</Button>
-        </Paper>
-      </Grid>
-    </Grid>
+    </div>
   );
 }
 
