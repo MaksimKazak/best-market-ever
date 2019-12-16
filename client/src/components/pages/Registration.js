b5d3931ba6f8d04766ab5451a9583903edbf6a4d
MaksimKazak <kazakmaksims@gmail.com>
2019-12-16 13:43:56 +0300

    Start implementing registration form

--- client/src/components/pages/Registration.js
+++ client/src/components/pages/Registration.js
@@ -5,18 +5,56 @@ import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
-function Registration() {
-  return (
-    <div>
-      <Typography variant='h5' className='registration-header'>Registration</Typography>
-      <Box className='registration-box'>
-        <TextField className='registration-input' label='username'/>
-        <TextField className='registration-input' label='email'/>
-        <TextField className='registration-input' label='password' type='password'/>
-        <Button color='primary' variant='contained'>Sign up!</Button>
-      </Box>
-    </div>
-  );
+class Registration extends React.Component {
+  state = {
+    username: '',
+    email: '',
+    password: ''
+  };
+
+  inputChangeHandler = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  signUpHandler(event) {
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <div>
+        <Typography variant='h5' className='registration-header'>Registration</Typography>
+        <Box className='registration-box'>
+          <form onSubmit={this.signUpHandler}>
+            <TextField className='registration-input'
+                       label='username'
+                       name='username'
+                       onChange={this.inputChangeHandler}
+                       value={this.state.username}
+            />
+            <TextField className='registration-input'
+                       label='email'
+                       name='email'
+                       onChange={this.inputChangeHandler}
+                       value={this.state.email}
+            />
+            <TextField className='registration-input'
+                       label='password'
+                       type='password'
+                       name='password'
+                       onChange={this.inputChangeHandler}
+                       value={this.state.password}
+            />
+            <Button type='submit' color='primary' variant='contained'>Sign up!</Button>
+          </form>
+        </Box>
+      </div>
+    );
+  }
 }
 
 export default Registration;
