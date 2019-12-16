8127cae5fe153cb65f96b6add3e12b0c8d3ef09d
MaksimKazak <kazakmaksims@gmail.com>
2019-12-16 16:24:36 +0300

    Send 401 error on bad credential

--- routes/api/User.js
+++ routes/api/User.js
@@ -53,18 +53,18 @@ router.post('/login', auth.optional, (req, res, next) => {
   }
 
   return passport.authenticate('local', { session: false }, (err, passportUser, info) => {
-    if(err) {
+    if (err) {
       return next(err);
     }
 
-    if(passportUser) {
+    if (passportUser) {
       const user = passportUser;
       user.token = passportUser.generateJWT();
 
       return res.json({ user: user.toAuthJSON() });
     }
 
-    return res.status(400).info;
+    return res.send(401);
   })(req, res, next);
 });
 
