296a24ce062cdb7121ce9e70602e2355379be464
MaksimKazak <kazakmaksims@gmail.com>
2019-12-23 14:17:04 +0300

    Add modal for choosing amount of items

--- client/src/components/elements/operations/Product.js
+++ client/src/components/elements/operations/Product.js
@@ -1,17 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import Grid from "@material-ui/core/Grid";
+import Modal from "@material-ui/core/Modal";
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
+import Slider from '@material-ui/core/Slider';
 
 function Product({ product }) {
+  let [open, setOpen] = useState(false);
+  let [amount, setAmount] = useState(1);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const changeAmountHandler = (event, val) => {
+    setAmount(val);
+  };
+
   return (
     <Grid item xs={12} sm={6} md={4} key={product.resource}>
       <Paper className='box box-small-spacing'>
         <Typography variant='h5' className='space-bottom'>{product.resource}</Typography>
         <Typography className='space-bottom'>Price: {product.price.toFixed(2)}$</Typography>
-        <Button variant='outlined' color='primary'>Buy</Button>
+        <Button variant='outlined' color='primary' onClick={handleOpen}>Buy</Button>
       </Paper>
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        className='modal'
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={open}>
+          <div className='modal-box'>
+            <h2 id="transition-modal-title">Choose amount</h2>
+            <p id="transition-modal-description">
+              <Slider
+                defaultValue={1}
+                aria-labelledby="transition-modal-title"
+                min={1}
+                max={100}
+                step={1}
+                valueLabelDisplay="auto"
+                onChange={changeAmountHandler}
+              />
+            </p>
+            <Button color='primary'>Buy</Button>
+          </div>
+        </Fade>
+      </Modal>
     </Grid>
   );
 }
