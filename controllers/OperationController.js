3ee87a25b2a467650f4cbfb91799c1da6a81560a
MaksimKazak <kazakmaksims@gmail.com>
2019-12-30 19:08:03 +0300

    Add some new operation api methods

--- controllers/OperationController.js
+++ controllers/OperationController.js
@@ -1,4 +1,6 @@
+const ProductRepository = require('../repositories/ProductRepository');
 const OperationRepository = require('../repositories/OperationRepository');
+const productRepository = new ProductRepository();
 const operationRepository = new OperationRepository();
 
 class OperationController {
@@ -26,6 +28,53 @@ class OperationController {
     } catch (err) {
       throw err;
     }
+  }
+
+  async profit(req, res) {
+    const { payload: { id } } = req;
+    let [operations, products] = await Promise.all([
+      operationRepository.find({ user: id }),
+      productRepository.all()
+    ]);
+    let responseObject = {
+      profit: operations.reduce((acc, { type, amount }) => {
+        return type === 'sold' ? acc + amount : acc - amount;
+      }, 0)
+    };
+    for (let product of products) {
+      responseObject[product.resource] = operations.reduce((acc, { type, amount, resource }) => {
+        if (resource === product.resource) {
+          return type === 'sold' ? acc + amount : acc - amount;
+        }
+        return acc;
+      }, 0);
+    }
+    return res.status(200).send(responseObject);
+  }
+
+  async recentActivities(req, res) {
+    // let dayEarlier = moment().subtract(1, 'day');
+    // // get operations for 1 day and group them by resource
+    // let recentOperations = _.groupBy(
+    //   operations.filter(operation => moment(operation.createdAt).isAfter(dayEarlier)),
+    //   'resource'
+    // );
+    //
+    // for (let resource in recentOperations) {
+    //   if (recentOperations.hasOwnProperty(resource)) {
+    //     // group grouped by resource operations by type(sold/bought)
+    //     recentOperations[resource] = _.groupBy(
+    //       recentOperations[resource],
+    //       'type'
+    //     );
+    //     // convert grouped arrays of operations into quantities of items
+    //     for (let type in recentOperations[resource]) {
+    //       if (recentOperations[resource].hasOwnProperty(type)) {
+    //         recentOperations[resource][type] = recentOperations[resource][type].reduce((acc, { quantity }) => acc + quantity, 0);
+    //       }
+    //     }
+    //   }
+    // }
   }
 }
 
--- routes/api/Operation.js
+++ routes/api/Operation.js
@@ -5,5 +5,7 @@ const controller = new OperationController();
 
 //GET operations collection
 router.get('/', auth.required, controller.index);
+router.get('/profit', auth.required, controller.profit);
+router.get('/recent-activities', auth.required, controller.recentActivities);
 
 module.exports = router;
